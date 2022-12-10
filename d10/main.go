d6006d00ccdc6b5f027e8fb5a86cb69945ff815a
Stephen Gardner <stephenbgardner@gmail.com>
2022-12-09 23:09:07 -0800

    Day 10

---
+++ d10/main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type (
+	Instr struct {
+		op      byte
+		arg     int
+		latency int
+	}
+	Canvas [][]byte
+)
+
+const (
+	opAddx = iota
+	opNoop
+
+	canvasHeight = 6
+	canvasWidth  = 40
+	spriteWidth  = 3
+)
+
+func getSchedule(lines []string) []Instr {
+	instr := make([]Instr, len(lines))
+	for i, line := range lines {
+		instr := &instr[i]
+		code := strings.Split(line, " ")
+		switch code[0] {
+		case "addx":
+			instr.op = opAddx
+			instr.arg, _ = strconv.Atoi(code[1])
+			instr.latency = 2
+		case "noop":
+			instr.op = opNoop
+			instr.latency = 1
+		}
+	}
+	return instr
+}
+
+func getCanvas() Canvas {
+	canvas := make([][]byte, canvasHeight)
+	for i := range canvas {
+		canvas[i] = make([]byte, canvasWidth)
+		for j := range canvas[i] {
+			canvas[i][j] = '.'
+		}
+	}
+	return canvas
+}
+
+func (canvas Canvas) Draw(cycle, x int) {
+	col := cycle % 40
+	if uint(x+spriteWidth-col-1) < spriteWidth {
+		canvas[cycle/40][col] = '#'
+	}
+}
+
+func (canvas Canvas) Print() {
+	for _, row := range canvas {
+		fmt.Println(string(row))
+	}
+}
+
+func render(canvas Canvas, schedule []Instr) int {
+	breakpoint := map[int]bool{
+		20:  true,
+		60:  true,
+		100: true,
+		140: true,
+		180: true,
+		220: true,
+	}
+	sum := 0
+	x := 1
+	for cycle := 1; len(schedule) > 0; cycle++ {
+		canvas.Draw(cycle, x)
+		if breakpoint[cycle] {
+			sum += cycle * x
+		}
+		curr := &schedule[0]
+		curr.latency--
+		if curr.latency == 0 {
+			schedule = schedule[1:]
+			switch curr.op {
+			case opAddx:
+				x += curr.arg
+			}
+		}
+	}
+	return sum
+}
+
+func main() {
+	data, _ := os.ReadFile("input")
+	lines := strings.Split(string(data[:len(data)-1]), "\n")
+	schedule := getSchedule(lines)
+	canvas := getCanvas()
+	fmt.Println("Part 1:", render(canvas, schedule)) // Expected: 15680
+	fmt.Println("Part 2:")                           // Expected: ZFBFHGUP
+	canvas.Print()
+}
---
+++ d10/main_test.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"bytes"
+	"strings"
+	"testing"
+)
+
+var testData = []string{
+	"addx 15",
+	"addx -11",
+	"addx 6",
+	"addx -3",
+	"addx 5",
+	"addx -1",
+	"addx -8",
+	"addx 13",
+	"addx 4",
+	"noop",
+	"addx -1",
+	"addx 5",
+	"addx -1",
+	"addx 5",
+	"addx -1",
+	"addx 5",
+	"addx -1",
+	"addx 5",
+	"addx -1",
+	"addx -35",
+	"addx 1",
+	"addx 24",
+	"addx -19",
+	"addx 1",
+	"addx 16",
+	"addx -11",
+	"noop",
+	"noop",
+	"addx 21",
+	"addx -15",
+	"noop",
+	"noop",
+	"addx -3",
+	"addx 9",
+	"addx 1",
+	"addx -3",
+	"addx 8",
+	"addx 1",
+	"addx 5",
+	"noop",
+	"noop",
+	"noop",
+	"noop",
+	"noop",
+	"addx -36",
+	"noop",
+	"addx 1",
+	"addx 7",
+	"noop",
+	"noop",
+	"noop",
+	"addx 2",
+	"addx 6",
+	"noop",
+	"noop",
+	"noop",
+	"noop",
+	"noop",
+	"addx 1",
+	"noop",
+	"noop",
+	"addx 7",
+	"addx 1",
+	"noop",
+	"addx -13",
+	"addx 13",
+	"addx 7",
+	"noop",
+	"addx 1",
+	"addx -33",
+	"noop",
+	"noop",
+	"noop",
+	"addx 2",
+	"noop",
+	"noop",
+	"noop",
+	"addx 8",
+	"noop",
+	"addx -1",
+	"addx 2",
+	"addx 1",
+	"noop",
+	"addx 17",
+	"addx -9",
+	"addx 1",
+	"addx 1",
+	"addx -3",
+	"addx 11",
+	"noop",
+	"noop",
+	"addx 1",
+	"noop",
+	"addx 1",
+	"noop",
+	"noop",
+	"addx -13",
+	"addx -19",
+	"addx 1",
+	"addx 3",
+	"addx 26",
+	"addx -30",
+	"addx 12",
+	"addx -1",
+	"addx 3",
+	"addx 1",
+	"noop",
+	"noop",
+	"noop",
+	"addx -9",
+	"addx 18",
+	"addx 1",
+	"addx 2",
+	"noop",
+	"noop",
+	"addx 9",
+	"noop",
+	"noop",
+	"noop",
+	"addx -1",
+	"addx 2",
+	"addx -37",
+	"addx 1",
+	"addx 3",
+	"noop",
+	"addx 15",
+	"addx -21",
+	"addx 22",
+	"addx -6",
+	"addx 1",
+	"noop",
+	"addx 2",
+	"addx 1",
+	"noop",
+	"addx -10",
+	"noop",
+	"noop",
+	"addx 20",
+	"addx 1",
+	"addx 2",
+	"addx 2",
+	"addx -6",
+	"addx -11",
+	"noop",
+	"noop",
+	"noop",
+}
+
+func TestPart1(t *testing.T) {
+	expected := 13140
+	if res := render(getCanvas(), getSchedule(testData)); res != expected {
+		t.Log("Input:", "\n\t"+strings.Join(testData, "\n\t"))
+		t.Fatalf("%d != %d (expected)", res, expected)
+	}
+
+}
+
+func TestPart2(t *testing.T) {
+	// My result is offset by one space?
+	expected := []string{
+		"##..##..##..##..##..##..##..##..##..##..",
+		"###...###...###...###...###...###...###.",
+		"####....####....####....####....####....",
+		"#####.....#####.....#####.....#####.....",
+		"######......######......######......####",
+		"#######.......#######.......#######.....",
+	}
+	canvas := getCanvas()
+	render(canvas, getSchedule(testData))
+	for y := range canvas {
+		for x := range canvas[y] {
+			if canvas[y][x] != expected[y][x] {
+				t.Log("Input:", "\n\t"+strings.Join(testData, "\n\t"))
+				res := append([]byte{'\n', '\t'}, bytes.Join(canvas, []byte{'\n', '\t'})...)
+				t.Log("Result:", string(res))
+				t.Log("Expected:", "\n\t"+strings.Join(expected, "\n\t"))
+				t.FailNow()
+			}
+		}
+	}
+}
